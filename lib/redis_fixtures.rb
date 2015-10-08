5e3f09068bc13d86049ba254e7208f0dedb743f0
Daniel Magliola <dmagliola@crystalgears.com>
2015-10-08 20:44:16 +0100

    Added a bit more docs, to a couple methods / modules that didn't have any

--- lib/redis_fixtures.rb
+++ lib/redis_fixtures.rb
@@ -4,3 +4,7 @@ require 'redis_fixtures/dump_fixtures'
 require 'redis_fixtures/load_fixtures'
 require 'redis_fixtures/minitest'
 require 'redis_fixtures/version'
+
+# Add fixtures to your Redis database, to test the parts of your code that need Redis to be more than a cache.
+module RedisFixtures
+end
--- lib/redis_fixtures/minitest.rb
+++ lib/redis_fixtures/minitest.rb
@@ -1,6 +1,7 @@
 # Module to set a before_setup Minitest hook, to reset Redis to the state specified by the fixture
 # before each test runs
 module MinitestRedisFixtures
+  # Clear Redis DB and load the fixture before the setup step of each test
   def before_setup
     super
     RedisFixtures.load_fixtures
